af8abe413fe1ae9a5a7064df82233a4a47f79d30
Gerard <Gerardosullivan.work@email.com>
2020-11-16 00:03:22 +0000

    I am attempted using the gridlayout and successfully created a 7 x 7 grid :) I will be changing this design but it is interesting seeing a concept and how it could develop into a working version. Its just not the version that I want to create

--- Connect4/Game.java
+++ Connect4/Game.java
@@ -1,8 +1,12 @@
 package Connect4;
 /* This is my attempt at making the game connect 4
-I intend to make a 7x6 grid with a scoreboard to keep track of the amount of games won
+I intend to make a 6x7 grid with a scoreboard to keep track of the amount of games won
 The program begins with a method which randomly generators which player will go first
 a second method will determine who goes second a third method will display the results of who goes first
+
+Day2:I am considering changing my counter class as I am beginning to thing that i will need to create multiple instances of that class so I might need
+to separate the player class and the counter and assign a counter to a player. I am going to start the grid to give me a chance to think about it.
+After messing with the grid i am not too happy with it. I need to come up with a sketch and a layout and a simple plan on how it will work
 (to be continued.....)
  */
 
@@ -15,6 +19,7 @@ public class Game {
         //This is simple code to see who goes first Red or yellow and this assigns the player number to
         //the colour of the counter that is going first the counters are also generated at this point
         //This can be used to see who went first. By default red is player one and yellow is player two
+        GameGridBoard board = new GameGridBoard();
 
         int playerTurn =playerPositionGenerator();
         Counter red =new Counter("Red",playerTurn);
@@ -23,6 +28,7 @@ public class Game {
         System.out.println(red.toString() +"\n");
         System.out.println(yellow.toString());
         displayWhoGoesFirst(red);
+
 
 
 
--- Connect4/GameGridBoard.java
+++ Connect4/GameGridBoard.java
@@ -1,4 +1,67 @@
 package Connect4;
 
-public class GameGridBoard {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class GameGridBoard extends JFrame implements MouseListener {
+    private JLabel jLabels[];
+    private int clickEventCounter;
+    private int result=-1;
+    private boolean gameOver=false;
+    private Color defaultColour = getBackground();
+
+    public GameGridBoard()
+    {
+       /* JFrame gameBoard = new JFrame();
+        gameBoard.setSize(750,650);
+        gameBoard.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameBoard.setPreferredSize(gameBoard.getSize());*/
+
+
+        //Create a Grid object with the first value = to the amount of rows the second value is columns
+        //the 3rd value is the horizontal gap and the 4th value is the vertical gap in the object
+        GridLayout gridlayout= new GridLayout(7,7,0,0);
+        FlowLayout flowLayout= new FlowLayout();
+        setLayout(gridlayout);
+
+        jLabels = new JLabel[49];
+
+        for(int i=0;i<jLabels.length;i++)
+        {
+            jLabels[i] = new JLabel("" +(i+1),SwingConstants.CENTER);
+            jLabels[i].setBorder(BorderFactory.createLineBorder(Color.BLUE, 7));
+            add(jLabels[i]);
+            jLabels[i].addMouseListener(this);
+        }
+
+        setSize(650,650);
+        setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
